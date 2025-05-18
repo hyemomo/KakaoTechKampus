924a3b5d7ec3ebbe6a2665b8c055c8f97a7258bd
hyemin <hyemin6647@naver.com>
2025-05-18 13:43:45 +0900

    state  불변성 연습

--- react_study/src/App.jsx
+++ react_study/src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from 'react';
 
 const App = () => {
-  const [text, setText]= useState("")
-  const handleChangeText=(e)=>{
-    setText(e.target.value)
-  }
+  const [dogs, setDogs] = useState(["말티즈"]);
+  const onClickhandler = () => {
+    // dogs.push("시고르자브종") 상태가 변경되었음을 리액트가 포착하지를 못함
+    setDogs([...dogs, "시고르자브종"]);
+    //state를 변경해주는 유일한 방법 => setState
+  };
+  console.log(dogs)
   return (
     <div>
-      <input onChange={handleChangeText} type="text" name="" id="" value={text} />
-      <h1>{text}</h1>
+      <button onClick={onClickhandler}>강아지 추가가</button>
+      <h1>{dogs}</h1>
     </div>
   );
-}
+};
 
-export default App
+export default App;

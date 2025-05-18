12e3ed503506135731da42295a215a7354ba1798
hyemin <hyemin6647@naver.com>
2025-05-18 13:44:49 +0900

    state  불변성 연습 2

--- react_study/src/App.jsx
+++ react_study/src/App.jsx
@@ -1,20 +1,22 @@
-import React from "react";
-import { useState } from 'react';
+import React, { useState } from "react";
 
-const App = () => {
-  const [dogs, setDogs] = useState(["말티즈"]);
-  const onClickhandler = () => {
-    // dogs.push("시고르자브종") 상태가 변경되었음을 리액트가 포착하지를 못함
-    setDogs([...dogs, "시고르자브종"]);
-    //state를 변경해주는 유일한 방법 => setState
+function GoodCounter() {
+  const [items, setItems] = useState([1, 2, 3]);
+
+  const addItem = () => {
+    // 새 배열을 생성하고 기존 항목을 복사한 후 새 항목 추가
+    setItems([...items, items.length + 1]); // 불변성 유지
   };
-  console.log(dogs)
+
   return (
     <div>
-      <button onClick={onClickhandler}>강아지 추가가</button>
-      <h1>{dogs}</h1>
+      <button onClick={addItem}>Add Item</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
-};
-
-export default App;
+}
+export default GoodCounter;

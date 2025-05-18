00290ebe5a8ba9f7698b68583c37432abad725f6
hyemin <hyemin6647@naver.com>
2025-05-18 13:11:40 +0900

    state

--- react_study/src/App.jsx
+++ react_study/src/App.jsx
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
+import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
-  const [name, setName] = useState("성민이");
-  //{ 변수: "성민이",
-  //변수를 변경하는 함수: function(){}
-  //}
+  const [text, setText]= useState("")
+  const handleChangeText=(e)=>{
+    setText(e.target.value)
+  }
   return (
     <div>
-      <h1>{name}</h1>
-      <button
-        onClick={() => {
-          setName("혜민이");
-        }}
-      >
-        이름바꾸기
-      </button>
+      <input onChange={handleChangeText} type="text" name="" id="" value={text} />
+      <h1>{text}</h1>
     </div>
   );
-};
+}
 
-export default App;
+export default App
